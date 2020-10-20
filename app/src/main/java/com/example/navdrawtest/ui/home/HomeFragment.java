b0f0a8efdffb1d3324b9989c5c5d82d1aef20ea0
Joe <joe@MacBook.lan>
2020-10-20 10:31:20 +0100

    Initial commit

---
+++ app/src/main/java/com/example/navdrawtest/ui/home/HomeFragment.java
@@ -0,0 +1,44 @@
+package com.example.navdrawtest.ui.home;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.navdrawtest.R;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class); // ignore view model stuff for now
+
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+
+
+        /*homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });*/
+
+        textView.setText("Test");
+
+        Log.d("debug_string", "Home fragment onCreateView");
+
+        return root;
+    }
+
+
+}
